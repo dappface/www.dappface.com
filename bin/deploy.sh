5b39b74ec0d16565f8e0656d94b0453ccc3e020f
Luke Sugiura <jun@dappface.com>
2019-10-28 22:36:26 +0100

    refactor deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -6,12 +6,10 @@ set -o xtrace
 
 if [ "${GITHUB_EVENT_NAME:-}" = "release" ] ; then
   export APP_ENV=prd
-elif [ "${GITHUB_EVENT_NAME:-}" = "push" ] ; then
-  if [ $(basename  "${GITHUB_REF:-}") = 'master' ] ; then
-    export APP_ENV=stg
-  else
-    export APP_ENV=dev
-  fi
+elif [ "${GITHUB_EVENT_NAME:-}" = "push" ] && [ $(basename  "${GITHUB_REF:-}") = 'master' ] ; then
+  export APP_ENV=stg
+else
+  export APP_ENV=dev
 fi
 
 ./node_modules/.bin/firebase use "$APP_ENV" --token="$FIREBASE_TOKEN"
