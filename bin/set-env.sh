3763d402b2e5dffcee9d409cd50d77ff5a8b9753
Luke Sugiura <jun@dappface.com>
2019-10-29 11:09:14 +0100

    pass DOMAIN env

--- bin/set-env.sh
+++ bin/set-env.sh
@@ -5,8 +5,11 @@ set -o xtrace
 
 if [ "$GITHUB_EVENT_NAME" = "release" ] ; then
   export APP_ENV=prd
+  export DOMAIN=dappface.com
 elif [ "$GITHUB_EVENT_NAME" = "push" ] && [ $(basename  "$GITHUB_REF") = 'master' ] ; then
   export APP_ENV=stg
+  export DOMAIN=stg.dappface.com
 else
   export APP_ENV=dev
+  export DOMAIN=dev.dappface.com
 fi
