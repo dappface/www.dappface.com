79d0e8c99b83e3e7e9aa74703e8b4342923368f2
Luke Sugiura <jun@dappface.com>
2019-11-01 15:04:58 +0100

    seperate hooked methods

--- src/components/provider/ethereum.tsx
+++ src/components/provider/ethereum.tsx
@@ -5,10 +5,21 @@ import {RawMethod, GetBalanceMethod} from './method'
 export function EthereumPlayground(): JSX.Element {
   return (
     <>
-      <RawMethod method='eth_accounts' />
-      <RawMethod method='eth_coinbase' />
-      <GetBalanceMethod />
-      <RawMethod method='eth_subscribe' params={['newHeads']} />
+      <div>
+        <h3>Hooked Methods</h3>
+        <div>
+          <RawMethod method='eth_accounts' />
+          <RawMethod method='eth_coinbase' />
+        </div>
+      </div>
+
+      <div>
+        <h3>Methods directly call remote node</h3>
+        <div>
+          <GetBalanceMethod />
+          <RawMethod method='eth_subscribe' params={['newHeads']} />
+        </div>
+      </div>
     </>
   )
 }
